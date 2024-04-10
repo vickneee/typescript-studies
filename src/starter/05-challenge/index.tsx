4738169d29523c102cac9a8744ae8965ec078463
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2024-04-10 20:03:24 +0300

    Initial commit

--- src/starter/05-challenge/index.tsx
+++ src/starter/05-challenge/index.tsx
@@ -1,11 +1,44 @@
-import React from "react";
+type BasicProfileCardProps = {
+    type: 'basic';
+    name: string;
+};
 
-function Component(): React.ReactElement {
+type AdvancedProfileCardProps = {
+    type: 'advanced';
+    name: string;
+    email: string;
+};
+type ProfileCardProps = BasicProfileCardProps | AdvancedProfileCardProps;
+function Component(props: ProfileCardProps) {
+    const { type, name } = props;
+    if (type === 'basic')
+        return (
+            <article className='alert alert-success'>
+                <h2>user : {name}</h2>
+            </article>
+        );
+
     return (
-        <div>
-            <h1>Hello</h1>
-        </div>
-    )
+        <article className='alert alert-danger'>
+            <h2>user : {name}</h2>
+            <h2>email : {props.email}</h2>
+        </article>
+    );
 }
-
 export default Component;
+
+// import './App.css'
+// import Component from './starter/05-challenge/index.tsx'
+//
+// function App() {
+//
+//     return (
+//         <main>
+//             <Component type='basic' name='Helle' />
+//             <Component type='advanced' name='Helle' email='test@gmail.com' />
+//
+//         </main>
+//     )
+// }
+//
+// export default App;
