7dbc19e6be286b1e74f252927e8caa448f1e352c
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2024-04-12 04:05:46 +0300

    Initial commit

--- src/starter/07-reducers/index.tsx
+++ src/starter/07-reducers/index.tsx
@@ -1,11 +1,39 @@
-import React from "react";
+import { useReducer } from 'react';
+import { counterReducer, initialState } from './reducer';
 
-function Component(): React.ReactElement {
+function Component() {
+    const [state, dispatch] = useReducer(counterReducer, initialState);
     return (
         <div>
-            <h1>Hello</h1>
-        </div>
-    )
-}
+            <h2>Count: {state.count}</h2>
+            <h2>Status: {state.status}</h2>
 
+            <div className='btn-container'>
+                <button onClick={() => dispatch({ type: 'increment' })} className='btn'>
+                    Increment
+                </button>
+                <button onClick={() => dispatch({ type: 'decrement' })} className='btn'>
+                    Decrement
+                </button>
+                <button onClick={() => dispatch({ type: 'reset' })} className='btn'>
+                    Reset
+                </button>
+            </div>
+            <div className='btn-container'>
+                <button
+                    onClick={() => dispatch({ type: 'setStatus', payload: 'active' })}
+                    className='btn'
+                >
+                    Set Status to Active
+                </button>
+                <button
+                    className='btn'
+                    onClick={() => dispatch({ type: 'setStatus', payload: 'inactive' })}
+                >
+                    Set Status to Inactive
+                </button>
+            </div>
+        </div>
+    );
+}
 export default Component;
